fc256c3691b061b404a3e6a769a05b6a2cff197e
Lawrenci01 <rlnodado@gbox.ncf.edu.ph>
2024-12-16 11:59:44 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,5 +45,4 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
-
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server started at 0.0.0.0:${PORT}`));
